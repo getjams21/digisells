ffcb7c66930352f05a9dba5cc8c6c7246c91ab59
Jay Marz <algoisticdevteam@gmail.com>
2014-07-31 02:06:51 +0800

    square styling

--- app/views/users/register.blade.php
+++ app/views/users/register.blade.php
@@ -6,8 +6,8 @@
 @section('content')
 <div clas="row" style="padding-top:80px;">
 	<div class="col-md-6 col-md-offset-3">
-		<div class="panel panel-primary">
-			<div class="panel-heading">
+		<div class="panel panel-primary square">
+			<div class="panel-heading square">
 	   			 <h3 class="panel-title" ><h4 style="margin-left:20px;"><b >Registration</b></h4></h3>
 	  		</div>
 	  	<div class="panel-body">
@@ -17,33 +17,33 @@
 				<hr>
 					<div class="form-group">
 						{{Form::label('firstName', 'First Name')}}
-						{{Form::text('firstName',null,['class'=>'form-control','required'=>'required'])}}
+						{{Form::text('firstName',null,['class'=>'form-control square','required'=>'required'])}}
 					</div>
 					<div class="form-group">
 						{{Form::label('lastName', 'Last Name')}}
-						{{Form::text('lastName',null,['class'=>'form-control','required'=>'required'])}}
+						{{Form::text('lastName',null,['class'=>'form-control square','required'=>'required'])}}
 					</div>
 					<div class="form-group">
 						{{Form::label('address', 'Address')}}
-						{{Form::textarea('address',null,['class'=>'form-control','required'=>'required'])}}
+						{{Form::textarea('address',null,['class'=>'form-control square','required'=>'required'])}}
 					</div>
 				<br>
 					<h4><b><i>Account Information</i></b></h4>
 				<hr>
 					<div class="form-group">
 						{{Form::label('email', 'Email')}}
-						{{Form::email('email',null,['class'=>'form-control','required'=>'required'])}}
+						{{Form::email('email',null,['class'=>'form-control square','required'=>'required'])}}
 					</div>
 					<div class="form-group">
 						{{Form::label('password', 'Password')}}
-						{{Form::password('password',['class'=>'form-control','required'=>'required'])}}
+						{{Form::password('password',['class'=>'form-control square','required'=>'required'])}}
 					</div>
 					<div class="form-group">
 						{{Form::label('passwordConfirmation','Retype Password')}}
-						{{Form::password('passwordConfirmation',['class'=>'form-control','required'=>'required'])}}
+						{{Form::password('passwordConfirmation',['class'=>'form-control square','required'=>'required'])}}
 					</div>
 					<div style=":30%;margin-left:60%;" >
-						{{ Form::Submit('Create Account',['class'=>'btn btn-primary']) }}
+						{{ Form::Submit('Create Account',['class'=>'btn btn-primary square']) }}
 					</div>	
 			</div>
 			{{ Form::close()}}			
