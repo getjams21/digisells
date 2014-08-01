8b465d7014f337f8d69eb55af522343b06fa9582
Jay Marz <algoisticdevteam@gmail.com>
2014-08-02 02:09:41 +0800

    Added selling options auction and direct selling

--- app/routes.php
+++ app/routes.php
@@ -10,17 +10,16 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
-<<<<<<< HEAD
+
+#Home
 Route::get('/',['as'=>'home','uses'=>'HomePageController@index']);
 Route::resource('page', 'HomePageController');
 Route::get('/register','UsersController@create');
 Route::resource('users', 'UsersController');
+
+#Selling
 Route::resource('/selling', 'SellingController');
 Route::resource('/auction', 'AuctionController');
-=======
-#Home
-Route::get('/',['as'=>'home','uses'=>'PageController@index']);
-Route::resource('page', 'PageController');
 
 #Registration
 Route::get('/register','UsersController@create')->before('guest');
@@ -30,4 +29,3 @@ Route::resource('users', 'UsersController');
 Route::get('login',['as' => 'login', 'uses' =>'SessionsController@create']);
 Route::get('logout',['as'=>'logout', 'uses' =>'SessionsController@destroy']);
 Route::resource('sessions', 'SessionsController',['only' => ['create','store','destroy']]);
->>>>>>> e0b563bee62efe696626d51e1b9acc0abbd53dd5
